8b11e4811f23d157f020503ec8f44d3f7549da2a
SimonPan <panruising@163.com>
2019-04-11 21:38:13 +0800

    1

---
+++ MahApps.Metro.IconPacks.Browser/ViewModels/IconPackViewModel.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace MahApps.Metro.IconPacks.Browser.ViewModels
+{
+    public class IconPackViewModel : ViewModelBase
+    {
+        private IEnumerable<IIconViewModel> _icons;
+        private ICollectionView _iconsCollectionView;
+        private string _filterText;
+        private IIconViewModel _selectedIcon;
+
+        public IconPackViewModel(MainViewModel mainViewModel, string caption, Type enumType, Type packType)
+        {
+            this.MainViewModel = mainViewModel;
+            this.Caption = caption;
+            this.Icons = new ObservableCollection<IIconViewModel>(GetIcons(enumType, packType).OrderBy(i => i.Name, StringComparer.InvariantCultureIgnoreCase));
+            this.PrepareFiltering();
+            this.SelectedIcon = this.Icons.First();
+        }
+
+        public IconPackViewModel(MainViewModel mainViewModel, string caption, Type[] enumTypes, Type[] packTypes)
+        {
+            this.MainViewModel = mainViewModel;
+
+            var allIcons = Enumerable.Empty<IIconViewModel>();
+            for (var counter = 0; counter < enumTypes.Length; counter++)
+            {
+                allIcons = allIcons.Concat(GetIcons(enumTypes[counter], packTypes[counter]));
+            }
+
+            this.Caption = caption;
+            this.Icons = new ObservableCollection<IIconViewModel>(allIcons.OrderBy(i => i.Name, StringComparer.InvariantCultureIgnoreCase));
+            this.PrepareFiltering();
+            this.SelectedIcon = this.Icons.First();
+        }
+
+        private void PrepareFiltering()
+        {
+            this._iconsCollectionView = CollectionViewSource.GetDefaultView(this.Icons);
+            this._iconsCollectionView.Filter = o => this.FilterIconsPredicate(this.FilterText, (IIconViewModel) o);
+        }
+
+        private bool FilterIconsPredicate(string filterText, IIconViewModel iconViewModel)
+        {
+            return string.IsNullOrWhiteSpace(filterText)
+                   || iconViewModel.Name.IndexOf(filterText, StringComparison.CurrentCultureIgnoreCase) >= 0
+                   || (!string.IsNullOrWhiteSpace(iconViewModel.Description) && iconViewModel.Description.IndexOf(filterText, StringComparison.CurrentCultureIgnoreCase) >= 0);
+        }
+
+        private static string GetDescription(Enum value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+            var attribute = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault() as DescriptionAttribute;
+            return attribute != null ? attribute.Description : value.ToString();
+        }
+
+        private static IEnumerable<IIconViewModel> GetIcons(Type enumType, Type packType)
+        {
+            return Enum.GetValues(enumType)
+                .OfType<Enum>()
+                .Select(k => GetIconViewModel(enumType, packType, k));
+        }
+
+        private static IIconViewModel GetIconViewModel(Type enumType, Type packType, Enum k)
+        {
+            var description = GetDescription(k);
+            return new IconViewModel()
+            {
+                Name = k.ToString(),
+                Description = description,
+                IconPackType = packType,
+                IconType = enumType,
+                Value = k
+            };
+        }
+
+        public MainViewModel MainViewModel { get; }
+
+        public string Caption { get; }
+
+        public IEnumerable<IIconViewModel> Icons
+        {
+            get { return _icons; }
+            set { Set(ref _icons, value); }
+        }
+
+        public string FilterText
+        {
+            get { return _filterText; }
+            set
+            {
+                if (Set(ref _filterText, value))
+                {
+                    this._iconsCollectionView.Refresh();
+                }
+            }
+        }
+
+        public IIconViewModel SelectedIcon
+        {
+            get { return _selectedIcon; }
+            set { Set(ref _selectedIcon, value); }
+        }
+    }
+
+    public interface IIconViewModel
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        Type IconPackType { get; set; }
+        Type IconType { get; set; }
+        object Value { get; set; }
+    }
+
+    public class IconViewModel : ViewModelBase, IIconViewModel
+    {
+        public IconViewModel()
+        {
+            this.CopyToClipboard =
+                new SimpleCommand
+                {
+                    CanExecuteDelegate = x => (x != null),
+                    ExecuteDelegate = x => Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        var icon = (IIconViewModel) x;
+                        var text = $"<iconPacks:{icon.IconPackType.Name} Kind=\"{icon.Name}\" />";
+                        Clipboard.SetDataObject(text);
+                    }))
+                };
+
+            this.CopyToClipboardAsContent =
+                new SimpleCommand
+                {
+                    CanExecuteDelegate = x => (x != null),
+                    ExecuteDelegate = x => Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        var icon = (IIconViewModel) x;
+                        var text = $"{{iconPacks:{icon.IconPackType.Name} Kind={icon.Name}}}";
+                        Clipboard.SetDataObject(text);
+                    }))
+                };
+
+            this.CopyToClipboardAsPathIcon =
+                new SimpleCommand
+                {
+                    CanExecuteDelegate = x => (x != null),
+                    ExecuteDelegate = x => Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        var icon = (IIconViewModel) x;
+                        // The UWP type is in WPF app not available
+                        var text = $"<iconPacks:{icon.IconPackType.Name.Replace("PackIcon", "PathIcon")} Kind=\"{icon.Name}\" />";
+                        Clipboard.SetDataObject(text);
+                    }))
+                };
+        }
+
+        public ICommand CopyToClipboard { get; }
+
+        public ICommand CopyToClipboardAsContent { get; }
+
+        public ICommand CopyToClipboardAsPathIcon { get; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Type IconPackType { get; set; }
+
+        public Type IconType { get; set; }
+
+        public object Value { get; set; }
+    }
+}
---
+++ MahApps.Metro.IconPacks.Browser/ViewModels/MainViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace MahApps.Metro.IconPacks.Browser.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private Dispatcher _dispatcher;
+        private string _filterText;
+
+        public MainViewModel(Dispatcher dispatcher)
+        {
+            this._dispatcher = dispatcher;
+            this.AppVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            this.IconPacks = new ObservableCollection<IconPackViewModel>(
+                new[]
+                {
+                    new IconPackViewModel(this, "Material", typeof(PackIconMaterialKind), typeof(PackIconMaterial)),
+                    new IconPackViewModel(this, "MaterialDesign", typeof(PackIconMaterialDesignKind), typeof(PackIconMaterialDesign)),
+                    new IconPackViewModel(this, "MaterialLight", typeof(PackIconMaterialLightKind), typeof(PackIconMaterialLight)),
+                    new IconPackViewModel(this, "FontAwesome", typeof(PackIconFontAwesomeKind), typeof(PackIconFontAwesome)),
+                    new IconPackViewModel(this, "Octicons", typeof(PackIconOcticonsKind), typeof(PackIconOcticons)),
+                    new IconPackViewModel(this, "Modern", typeof(PackIconModernKind), typeof(PackIconModern)),
+                    new IconPackViewModel(this, "Entypo+", typeof(PackIconEntypoKind), typeof(PackIconEntypo)),
+                    new IconPackViewModel(this, "SimpleIcons", typeof(PackIconSimpleIconsKind), typeof(PackIconSimpleIcons)),
+                    new IconPackViewModel(this, "WeatherIcons", typeof(PackIconWeatherIconsKind), typeof(PackIconWeatherIcons)),
+                    new IconPackViewModel(this, "Typicons", typeof(PackIconTypiconsKind), typeof(PackIconTypicons)),
+                    new IconPackViewModel(this, "FeatherIcons", typeof(PackIconFeatherIconsKind), typeof(PackIconFeatherIcons)),
+                    new IconPackViewModel(this, "Ionicons", typeof(PackIconIoniconsKind), typeof(PackIconIonicons)),
+                    new IconPackViewModel(this, "JamIcons", typeof(PackIconJamIconsKind), typeof(PackIconJamIcons)),
+                    new IconPackViewModel(this, "All",
+                        new[]
+                        {
+                            typeof(PackIconMaterialKind),
+                            typeof(PackIconMaterialDesignKind),
+                            typeof(PackIconMaterialLightKind),
+                            typeof(PackIconFontAwesomeKind),
+                            typeof(PackIconOcticonsKind),
+                            typeof(PackIconModernKind),
+                            typeof(PackIconEntypoKind),
+                            typeof(PackIconSimpleIconsKind),
+                            typeof(PackIconWeatherIconsKind),
+                            typeof(PackIconTypiconsKind),
+                            typeof(PackIconFeatherIconsKind),
+                            typeof(PackIconIoniconsKind),
+                            typeof(PackIconJamIconsKind)
+                        },
+                        new[]
+                        {
+                            typeof(PackIconMaterial),
+                            typeof(PackIconMaterialDesign),
+                            typeof(PackIconMaterialLight),
+                            typeof(PackIconFontAwesome),
+                            typeof(PackIconOcticons),
+                            typeof(PackIconModern),
+                            typeof(PackIconEntypo),
+                            typeof(PackIconSimpleIcons),
+                            typeof(PackIconWeatherIcons),
+                            typeof(PackIconTypicons),
+                            typeof(PackIconFeatherIcons),
+                            typeof(PackIconIonicons),
+                            typeof(PackIconJamIcons)
+                        })
+                });
+            this.IconPacksVersion = Assembly.GetAssembly(typeof(PackIconMaterial)).GetName().Version.ToString();
+            this.GoToGitHubCommand =
+                new SimpleCommand
+                {
+                    CanExecuteDelegate = x => true,
+                    ExecuteDelegate = x => System.Diagnostics.Process.Start("https://github.com/MahApps/MahApps.Metro.IconPacks")
+                };
+        }
+
+        public ObservableCollection<IconPackViewModel> IconPacks { get; set; }
+
+        public string AppVersion { get; }
+
+        public string IconPacksVersion { get; }
+
+        public ICommand GoToGitHubCommand { get; }
+
+        public string FilterText
+        {
+            get { return _filterText; }
+            set
+            {
+                if (Set(ref _filterText, value))
+                {
+                    foreach (var iconPack in this.IconPacks)
+                    {
+                        this._dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => iconPack.FilterText = value));
+                    }
+                }
+            }
+        }
+    }
+}
